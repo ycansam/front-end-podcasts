631ad1ba944d174f9ce1b278b90f8230827be4e0
Yeray Candel <ycansam@epsg.upv.es>
2023-03-11 22:38:21 +0100

    creado test navegacion a un podcast

--- __tests__/PodcastList.test.tsx
+++ __tests__/PodcastList.test.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react'
 import PodcastsList from '@/components/TopPodcastsSection/PodcastsList/PodcastsList';
+import { useRouter } from 'next/router';
+import mockRouter from 'next-router-mock';
+import { MemoryRouterProvider } from 'next-router-mock/MemoryRouterProvider';
 
 describe('PodcastsList component', () => {
     const podcasts: any = [
@@ -19,18 +22,23 @@ describe('PodcastsList component', () => {
     ];
 
     const setup = () => {
-        const container = render(<PodcastsList podcasts={podcasts} />)
+        const container = render(<PodcastsList podcasts={podcasts} />, { wrapper: MemoryRouterProvider })
         const ul = screen.getByTestId('test-ul');
         const listElements = screen.getAllByTestId('test-li');
         return { ul, listElements, ...container, }
     }
 
-
     it('should render a list of podcasts', () => {
         const { ul, listElements, container } = setup();
-
-        expect(container).toBeInTheDocument();
+        const links = screen.getAllByRole('link');
         expect(ul).toBeInTheDocument();
-        expect(listElements.length).toBe(podcasts.length);
+        expect(container).toBeInTheDocument();
+    });
+
+    it('should go to podcast/1', () => {
+        const { container } = setup();
+        const links = screen.getAllByRole('link');
+        fireEvent.click(links[0]);
+        expect(mockRouter.asPath).toEqual('/podcast/1')
     });
 });
